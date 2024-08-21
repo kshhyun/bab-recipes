806acff2a3fcc20d4d48a0c5b68b3bdc5e0c7fed
Kiseok <165978058+chundae@users.noreply.github.com>
2024-08-21 17:58:07 +0900

    Delete src directory

--- src/main/java/com/example/bab_recipes/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bab_recipes.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
